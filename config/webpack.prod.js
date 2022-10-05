0cbd27c9cb17ecdd35fd6f3021e4661fe6af3999
Bryan Arriaza <bryan.arriaza.g@gmail.com>
2022-10-05 11:32:15 -0600

    chore: setting domain

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -4,18 +4,21 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
+const PORT = 3000;
+const DOMAIN_USERS = 'http://localhost:3001';
+
 const prodConfig = {
   mode: 'production',
   output: {
-    filename: '[name].[contenthash].js',
-    publicPath: '/',
+    filename: 'static/js/[name].[contenthash].js',
+    publicPath: `http://localhost:${PORT}/`,
   },
   plugins: [
     new ModuleFederationPlugin({
       name: 'container',
       filename: 'containerRemoteEntry.js',
       remotes: {
-        users: 'users@http://localhost:3001/usr/share/nginx/html/usersRemoteEntry.js',
+        users: `users@${DOMAIN_USERS}/usersRemoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
